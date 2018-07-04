20032fb44342ef89ce2b691a28935eb86c23153a
gangadhararao <gangadhararao.nukarapu@gmail.com>
2018-07-05 02:00:45 +0530

    New Project GitMavenJavaDemo code updates

---
+++ GitMavenJavaDemo/src/main/java/com/hcl/gangadhar/GitMavenJavaDemo.java
@@ -0,0 +1,14 @@
+package com.hcl.gangadhar;
+
+public class GitMavenJavaDemo {
+
+	public GitMavenJavaDemo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("GitMavenJavaDemo");
+	}
+
+}
